5b93050a6ea360a87ed4fd013a12853b00ea4d84
Onur DÖKER <odoker@msn.com>
2025-08-30 20:24:54 +0300

    Integrate Transaction History into App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import "./App.css";
-import Profile from "./components/Profile/Profile";
 import userData from "./data/userData.json";
+import Profile from "./components/Profile/Profile";
 import friends from "./data/friends.json";
 import FriendList from "./components/FriendList/FriendList";
+import transactions from "./data/transaction.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 
 function App() {
   return (
-    <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
+      <>
+        <Profile
+            name={userData.username}
+            tag={userData.tag}
+            location={userData.location}
+            image={userData.avatar}
+            stats={userData.stats}
+        />
 
-      <FriendList friends={friends} />
-    </>
+        <FriendList friends={friends}/>
+
+        <TransactionHistory transactions={transactions}/>
+      </>
   );
 }
 
